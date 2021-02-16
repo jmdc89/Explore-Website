d5ce7f667996a9091f3c03902688713c193c62d4
Jose Manuel <jmdcarq89@gmail.com>
2021-02-16 20:01:27 +0100

    Hero Section

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./HeroSection.scss"
+
+function HeroSection() {
+    return (
+        <div className="hero-container">
+            <h1>Travel More</h1>
+            <p>Plan yout next trip today</p>
+            <div className="hero-btns">
+                <button>Get Started</button>
+            </div>
+        </div>
+    )
+}
+
+export default HeroSection
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import HeroSection from '../components/HeroSection';
 import Layout from "../components/layout"
 // import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
       {/* <SEO title="Home" /> */}
-      <h1>Home</h1>
+      <HeroSection/>
   </Layout>
 )
 
