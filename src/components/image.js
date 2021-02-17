8cf9e03f7f2cf00de05a4c092174feeeb96e881b
Jose Manuel <jmdcarq89@gmail.com>
2021-02-17 13:32:52 +0100

    image css

--- src/components/image.js
+++ src/components/image.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
+import "./image.scss"
 
 const Image = () => {
     const data = useStaticQuery(graphql`
